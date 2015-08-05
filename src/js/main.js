9a39adb0182ca10640c3e4f6b11c8c9448b8b70f
Jack Wilsdon <jack.wilsdon2@gmail.com>
2015-08-05 01:45:08 +0100

    Add showAlert method and basic error handling

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,81 @@
-function endpointCallback(fields, response) {
+function showAlert(clazz, text, baseElement) {
+  var div = document.createElement('div');
 
+  div.className = "alert " + clazz + " alert-dismissible";
+
+  var button = document.createElement('span');
+
+  button.className = "close";
+  button.setAttribute("data-dismiss", "alert");
+
+  button.innerHTML = "&times;";
+
+  div.appendChild(button);
+
+  div.innerHTML += text;
+
+  baseElement.appendChild(div);
 }
 
-function defineEndpoint(name, label, fields, form, addressOverride) {
+function endpointCallback(fields, responseValues, alertContainer, response) {
+  if (response.hasOwnProperty("error")) {
+    if (response.error_type === "FieldError" || response.error_type === "NoFieldError") {
+      var fieldName = fields[response.error_field],
+          message = "Unknown error";
+
+      if (fieldName !== null && fieldName !== undefined) {
+        showAlert("alert-danger", fieldName + " " + response.message, alertContainer);
+      } else {
+        showAlert("alert-danger", "Sorry, something went wrong!", alertContainer);
+        throw new Error("invalid response from server");
+      }
+    } else {
+      showAlert("alert-danger", "Sorry, something went wrong!", alertContainer);
+      throw new Error("unable to handle error `" + response.error_type + "' from server");
+    }
+  } else if (response.hasOwnProperty("result")) {
+    var result = response.result,
+        messages = [];
+
+    for (var fieldName in responseValues) {
+      var fieldValue = result[fieldName],
+          name = fieldName;
+
+      if (fieldValue === null || fieldValue === undefined) {
+        showAlert("alert-danger", "Sorry, something went wrong!", alertContainer);
+        throw new Error("field `" + fieldName + "' not found in response");
+      }
+
+      if (typeof(fieldValue) === "function") {
+        name = fieldValue(fieldName);
+      } else if (typeof(fieldValue) === "object") {
+        if (fieldValue.hasOwnProperty("prefix")) {
+          name = fieldValue.prefix + parsedValue;
+        }
+
+        if (fieldValue.hasOwnProperty("suffix")) {
+          name += fieldValue.suffix;
+        }
+      }
+
+      messages.push("<b>" + name + "</b>: " + fieldValue);
+    }
+
+    showAlert("alert-success", message.join("<br />"), alertContainer);
+  }
+}
+
+function defineEndpoint(name, label, fields, responseValues, form, alertContainer, addressOverride) {
   var endpoint = new FormEndpoint(name, label, Object.keys(fields)),
-      formElement = document.getElementById(form);
+      formElement = document.getElementById(form),
+      alertElement = document.getElementById(alertContainer);
 
   if (formElement === null || formElement === undefined) {
     throw new Error("missing form `" + form + "'");
+  }
+
+  if (alertElement === null || alertElement === undefined) {
+    throw new Error("missing alert container `" + alertContainer + "'");
   }
 
   formElement.onsubmit = function(e) {
@@ -19,6 +87,6 @@ function defineEndpoint(name, label, fields, form, addressOverride) {
       address = addressOverride;
     }
 
-    endpoint.request(address, endpointCallback.bind(null, fields));
+    endpoint.request(address, endpointCallback.bind(null, fields, responseValues, alertElement));
   };
 }
